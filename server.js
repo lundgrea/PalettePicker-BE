bc25e14761c5c875de1e0b265e3b27a4a3e58bd1
Ann Cerveny <cervann11@gmail.com>
2019-10-07 15:39:46 -0600

    Setup Knex and Express connector files
    Co-authored-by: Alyssa Lundgren <lundgrea@gmail.com>

--- server.js
+++ server.js
@@ -1,5 +1,8 @@
 const express = require('express')
 const app = express()
+const environment = process.env.NODE_ENV || 'development';
+const configuration = require('./knexfile')[environment]
+const database = require('knex')(configuration)
 
 app.use(express.json())
 
