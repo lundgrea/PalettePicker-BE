7ab8c84f3199383d0a0236f4885852a296b025d0
Ann Cerveny <cervann11@gmail.com>
2019-10-08 16:42:54 -0600

    Setup server tests involving get; Setup get functionality in app.js
    Co-authored-by: Alyssa Lundgren <lundgrea@gmail.com>

--- app.js
+++ app.js
@@ -12,10 +12,34 @@ app.get('/', (request, response) => {
   response.send("The colors have been gotten.")
 })
 
-app.get('/students', async (request, response) => {
-  const students = await database('students').select();
+app.get('/folders', async (request, response) => {
+  const folders = await database('folders').select();
 
-  return response.status(200).json(students);
+  return response.status(200).json(folders);
+})
+
+app.get('/folders/:id', async (request, response) => {
+  const folder = await database('folders').where('id', request.params.id).select();
+  if(folder.length) {
+    return response.status(200).json(folder)
+  } else {
+    return response.status(404).json({error: 'Folder not found'})
+  }
+})
+
+app.get('/palettes', async (request, response) => {
+  const palettes = await database('palettes').select();
+
+  return response.status(200).json(palettes);
+})
+
+app.get('/palettes/:id', async (request, response) => {
+  const palette = await database('palettes').where('id', request.params.id).select();
+  if(palette.length) {
+    return response.status(200).json(palette)
+  } else {
+    return response.status(404).json({error: 'Palette not found'})
+  }
 })
 
 module.exports = app;
--- app.test.js
+++ app.test.js
@@ -6,11 +6,81 @@ const configuration = require('./knexfile')[environment]
 const database = require('knex')(configuration)
 
 describe('Server', () => {
+
+  beforeEach(async () => {
+    await database.seed.run()
+  });
+
   describe('init', () => {
     it('should return a 200 status', async () => {
       const res = await request(app).get('/')
       expect(res.status).toBe(200)
     })
+  })
 
+  describe('GET /folders', () => {
+    it('should return a 200 and all of the folders', async () => {
+      const expectedFolders = await database('folders').select()
+  
+      const res = await request(app).get('/folders')
+      const folders = res.body
+  
+      expect(res.status).toBe(200)
+      expect(folders).toEqual(expectedFolders)
+    })
+  })
+
+  describe('GET /folders/:id', () => {
+    it('should return a 200 and a single folder if the folder exists', async () => {
+      const expectedFolder = await database('folders').first()
+      const id = expectedFolder.id
+  
+      const res = await request(app).get(`/folders/${id}`)
+      const result = res.body[0]
+  
+      expect(res.status).toBe(200)
+      expect(result).toEqual(expectedFolder)
+    })
+
+    it('should return a 404 and the message "Folder not found"', async () => {
+      const invalidId = -1;
+      const response = await request(app).get(`/folders/${invalidId}`)
+
+      expect(response.status).toBe(404)
+      expect(response.body.error).toEqual('Folder not found')
+    })
+  })
+
+  describe('GET /palettes', () => {
+    it('should return a 200 and all of the palettes', async () => {
+      const expectedPalettes = await database('palettes').select()
+  
+      const res = await request(app).get('/palettes')
+      const palettes = res.body
+  
+      expect(res.status).toBe(200)
+      expect(palettes).toEqual(expectedPalettes)
+    })
+  })
+
+  describe('GET /palettes/:id', () => {
+    it('should return a 200 and a single palette if the palette exists', async () => {
+      const expectedPalette = await database('palettes').first()
+      const id = expectedPalette.id
+  
+      const res = await request(app).get(`/palettes/${id}`)
+      const result = res.body[0]
+  
+      expect(res.status).toBe(200)
+      expect(result).toEqual(expectedPalette)
+    })
+
+    it('should return a 404 and the message "Palette not found"', async () => {
+      const invalidId = -1;
+      const response = await request(app).get(`/palettes/${invalidId}`)
+
+      expect(response.status).toBe(404)
+      expect(response.body.error).toEqual('Palette not found')
+    })
   })
 })
