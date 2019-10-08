a4853e9f91bf8a12e7cd6be1ea420fffed4b1452
Alyssa Lundgren <38546045+lundgrea@users.noreply.github.com>
2019-10-08 14:13:27 -0600

    seed data (#3)
    * Begin laying out seed data outline
    
    * Add mock folder and palette data
    
    * Seed database
    
    Co-authored-by: Ann Cerveny <cervann11@gmail.com>

---
+++ folderData.js
@@ -0,0 +1,149 @@
+const folderData = [
+  {
+    name: "Bold",
+    palettes : [
+      {
+        name: "Autumn Bliss",
+        c1: "#586BA4",
+        c2: "#324376",
+        c3: "#F5DD90",
+        c4: "#F68E5F",
+        c5: "#F76C5E"
+      },
+      {
+        name: "Vegas",
+        c1: "#04E762",
+        c2: "#F5B700",
+        c3: "#00A1E4",
+        c4: "#DC0073",
+        c5: "#89FC00"
+      },
+      {
+        name: "Shout From the Rooftops",
+        c1: "#050505",
+        c2: "#004FFF",
+        c3: "#31AFD4",
+        c4: "#902D41",
+        c5: "#FFOO7F"
+      }
+    ]
+  },
+  {
+    name: "Shy",
+    palettes: [
+      {
+        name: "Violetta",
+        c1: "#586BA4",
+        c2: "#919098",
+        c3: "#BA8F95",
+        c4: "CAB6CD",
+        c5: "CFBCDF"
+      },
+      {
+        name: "Blushing",
+        c1: "#F2CCC3",
+        c2: "#E78F8E",
+        c3: "#FFE6E8",
+        c4: "#ACD8AA",
+        c5: "#F48498"
+      }
+    ]
+  },
+  {
+    name: "Animals",
+    palettes: [
+      {
+        name: "Whale Life",
+        c1: "#586BA4",
+        c2: "#594157",
+        c3: "#A0D2DB",
+        c4: "#BEE7E8",
+        c5: "#CFBCDF"
+      },
+      {
+        name: "SLOTH",
+        c1: "#C4BBAF",
+        c2: "#A5978B",
+        c3: "#5C4742",
+        c4: "#8D5B4C",
+        c5: "#5A2A27"
+      },
+      {
+        name: "Frogish",
+        c1: "#4F345A",
+        c2: "#5D4E6D",
+        c3: "#8FA998",
+        c4: "#9CBFA7",
+        c5: "#C9F299"
+      }
+    ]
+  },
+  {
+    name: "Monster Mash",
+    palettes: [
+      {
+        name: "Ghosted",
+        c1: "#C4E7D4",
+        c2: "#C4DACF",
+        c3: "#B9C0DA",
+        c4: "#998DA0",
+        c5: "#63585E"
+      },
+      {
+        name: "Moldy",
+        c1: "#94AE89",
+        c2: "#A8BCA1",
+        c3: "#CODA74",
+        c4: "#BEEDAA",
+        c5: "#D5FFD9"
+      },
+      {
+        name: "Purple Rain",
+        c1: "#7A5C61",
+        c2: "#E8F0FF",
+        c3: "#CA7DF9",
+        c4: "#6874E8",
+        c5: "#392759"
+      }
+    ]
+  },
+  {
+    name: "Winding Down",
+    palettes: [
+      {
+        name: "Tea Time",
+        c1: "#870058",
+        c2: "#A4303F",
+        c3: "#F2DOA4",
+        c4: "#FFECCC",
+        c5: "#C8D6AF"
+      },
+      {
+        name: "Snuggly",
+        c1: "#8DA1B9",
+        c2: "#95ADB6",
+        c3: "#CBB3BF",
+        c4: "#DBC7BE",
+        c5: "#EF959C"
+      },
+      {
+        name: "Winter Warmer",
+        c1: "#DB5461",
+        c2: "#686963",
+        c3: "#8AA29E",
+        c4: "#3D5467",
+        c5: "#F1EDEE"
+      },
+      {
+        name: "Purple Haze",
+        c1: "#E2CFEA",
+        c2: "#6247AA",
+        c3: "#CA7DF9",
+        c4: "#102B3F",
+        c5: "#062726"
+      }
+    ]
+  }
+]
+
+module.exports = folderData
