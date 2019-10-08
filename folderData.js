e8ebf01b26260877b28097c5e3992ae9d724eb0d
Alyssa Lundgren <lundgrea@gmail.com>
2019-10-08 08:04:29 -0600

    Begin laying out seed data outline

---
+++ folderData.js
@@ -0,0 +1,149 @@
+const folderData = [
+  {
+    name: "Bold",
+    palettes : [
+      {
+        name: "Autumn Bliss",
+        c1: "#586BA4",
+        c2: "#324376",
+        c3: "#F5DD90",
+        c4: "#F68E5F",
+        c5: "#F76C5E"
+      },
+      {
+        name: "",
+        c1: "",
+        c2: "",
+        c3: "",
+        c4: "",
+        c5: ""
+      },
+      {
+        name: "",
+        c1: "",
+        c2: "",
+        c3: "",
+        c4: "",
+        c5: ""
+      }
+    ]
+  },
+  {
+    name: "Shy",
+    palettes: [
+      {
+        name: "Violetta",
+        c1: "#586BA4",
+        c2: "#919098",
+        c3: "#BA8F95",
+        c4: "CAB6CD",
+        c5: "CFBCDF"
+      },
+      {
+        name: "",
+        c1: "",
+        c2: "",
+        c3: "",
+        c4: "",
+        c5: ""
+      },
+      {
+        name: "",
+        c1: "",
+        c2: "",
+        c3: "",
+        c4: "",
+        c5: ""
+      }
+    ]
+  },
+  {
+    name: "Animals",
+    palettes: [
+      {
+        name: "",
+        c1: "",
+        c2: "",
+        c3: "",
+        c4: "",
+        c5: ""
+      },
+      {
+        name: "",
+        c1: "",
+        c2: "",
+        c3: "",
+        c4: "",
+        c5: ""
+      },
+      {
+        name: "",
+        c1: "",
+        c2: "",
+        c3: "",
+        c4: "",
+        c5: ""
+      }
+    ]
+  },
+  {
+    name: "Monster Mash",
+    palettes: [
+      {
+        name: "Ghosted",
+        c1: "",
+        c2: "",
+        c3: "",
+        c4: "",
+        c5: ""
+      },
+      {
+        name: "",
+        c1: "",
+        c2: "",
+        c3: "",
+        c4: "",
+        c5: ""
+      },
+      {
+        name: "",
+        c1: "",
+        c2: "",
+        c3: "",
+        c4: "",
+        c5: ""
+      }
+    ]
+  },
+  {
+    name: "Winding Down",
+    palettes: [
+      {
+        name: "",
+        c1: "",
+        c2: "",
+        c3: "",
+        c4: "",
+        c5: ""
+      },
+      {
+        name: "",
+        c1: "",
+        c2: "",
+        c3: "",
+        c4: "",
+        c5: ""
+      },
+      {
+        name: "",
+        c1: "",
+        c2: "",
+        c3: "",
+        c4: "",
+        c5: ""
+      }
+    ]
+  }
+]
+
+module.exports = folderData
