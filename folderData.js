8c1b06d29c312e1bee461db35919f014e83224a0
Alyssa Lundgren <lundgrea@gmail.com>
2019-10-08 11:24:17 -0600

    Add mock folder and palette data

--- folderData.js
+++ folderData.js
@@ -11,20 +11,20 @@ const folderData = [
         c5: "#F76C5E"
       },
       {
-        name: "",
-        c1: "",
-        c2: "",
-        c3: "",
-        c4: "",
-        c5: ""
+        name: "Vegas",
+        c1: "#04E762",
+        c2: "#F5B700",
+        c3: "#00A1E4",
+        c4: "#DC0073",
+        c5: "#89FC00"
       },
       {
-        name: "",
-        c1: "",
-        c2: "",
-        c3: "",
-        c4: "",
-        c5: ""
+        name: "Shout From the Rooftops",
+        c1: "#050505",
+        c2: "#004FFF",
+        c3: "#31AFD4",
+        c4: "#902D41",
+        c5: "#FFOO7F"
       }
     ]
   },
@@ -40,20 +40,12 @@ const folderData = [
         c5: "CFBCDF"
       },
       {
-        name: "",
-        c1: "",
-        c2: "",
-        c3: "",
-        c4: "",
-        c5: ""
-      },
-      {
-        name: "",
-        c1: "",
-        c2: "",
-        c3: "",
-        c4: "",
-        c5: ""
+        name: "Blushing",
+        c1: "#F2CCC3",
+        c2: "#E78F8E",
+        c3: "#FFE6E8",
+        c4: "#ACD8AA",
+        c5: "#F48498"
       }
     ]
   },
@@ -61,28 +53,28 @@ const folderData = [
     name: "Animals",
     palettes: [
       {
-        name: "",
-        c1: "",
-        c2: "",
-        c3: "",
-        c4: "",
-        c5: ""
+        name: "Whale Life",
+        c1: "#586BA4",
+        c2: "#594157",
+        c3: "#A0D2DB",
+        c4: "#BEE7E8",
+        c5: "#CFBCDF"
       },
       {
-        name: "",
-        c1: "",
-        c2: "",
-        c3: "",
-        c4: "",
-        c5: ""
+        name: "SLOTH",
+        c1: "#C4BBAF",
+        c2: "#A5978B",
+        c3: "#5C4742",
+        c4: "#8D5B4C",
+        c5: "#5A2A27"
       },
       {
-        name: "",
-        c1: "",
-        c2: "",
-        c3: "",
-        c4: "",
-        c5: ""
+        name: "Frogish",
+        c1: "#4F345A",
+        c2: "#5D4E6D",
+        c3: "#8FA998",
+        c4: "#9CBFA7",
+        c5: "#C9F299"
       }
     ]
   },
@@ -91,27 +83,27 @@ const folderData = [
     palettes: [
       {
         name: "Ghosted",
-        c1: "",
-        c2: "",
-        c3: "",
-        c4: "",
-        c5: ""
+        c1: "#C4E7D4",
+        c2: "#C4DACF",
+        c3: "#B9C0DA",
+        c4: "#998DA0",
+        c5: "#63585E"
       },
       {
-        name: "",
-        c1: "",
-        c2: "",
-        c3: "",
-        c4: "",
-        c5: ""
+        name: "Moldy",
+        c1: "#94AE89",
+        c2: "#A8BCA1",
+        c3: "#CODA74",
+        c4: "#BEEDAA",
+        c5: "#D5FFD9"
       },
       {
-        name: "",
-        c1: "",
-        c2: "",
-        c3: "",
-        c4: "",
-        c5: ""
+        name: "Purple Rain",
+        c1: "#7A5C61",
+        c2: "#E8F0FF",
+        c3: "#CA7DF9",
+        c4: "#6874E8",
+        c5: "#392759"
       }
     ]
   },
@@ -119,28 +111,36 @@ const folderData = [
     name: "Winding Down",
     palettes: [
       {
-        name: "",
-        c1: "",
-        c2: "",
-        c3: "",
-        c4: "",
-        c5: ""
+        name: "Tea Time",
+        c1: "#870058",
+        c2: "#A4303F",
+        c3: "#F2DOA4",
+        c4: "#FFECCC",
+        c5: "#C8D6AF"
       },
       {
-        name: "",
-        c1: "",
-        c2: "",
-        c3: "",
-        c4: "",
-        c5: ""
+        name: "Snuggly",
+        c1: "#8DA1B9",
+        c2: "#95ADB6",
+        c3: "#CBB3BF",
+        c4: "#DBC7BE",
+        c5: "#EF959C"
       },
       {
-        name: "",
-        c1: "",
-        c2: "",
-        c3: "",
-        c4: "",
-        c5: ""
+        name: "Winter Warmer",
+        c1: "#DB5461",
+        c2: "#686963",
+        c3: "#8AA29E",
+        c4: "#3D5467",
+        c5: "#F1EDEE"
+      },
+      {
+        name: "Purple Haze",
+        c1: "#E2CFEA",
+        c2: "#6247AA",
+        c3: "#CA7DF9",
+        c4: "#102B3F",
+        c5: "#062726"
       }
     ]
   }
