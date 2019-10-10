58d15106951f3d9d56b0387e56d61ea2e2ddc990
Ann Cerveny <cervann11@gmail.com>
2019-10-10 11:50:37 -0600

    Update knexfile production obj client key with string of pg
    Co-authored-by: Alyssa Lundgren <lundgrea@gmail.com>

--- knexfile.js
+++ knexfile.js
@@ -22,7 +22,7 @@ module.exports = {
     useNullAsDefault: true,
   },
   production: {
-    client: pg,
+    client: 'pg',
     connection: process.env.DATABASE_URL + `?ssl=true`, 
     migrations: {
       directory: "./db/migrations"
